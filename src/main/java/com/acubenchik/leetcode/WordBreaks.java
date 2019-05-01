76a78e8fe02540b7692100e3c805010ca42c00e5
acubenchik <alex.usavchenko@gmail.com>
2019-05-01 13:35:58 +0200

    []: some algorithms;

---
+++ src/main/java/com/acubenchik/leetcode/WordBreaks.java
@@ -0,0 +1,31 @@
+package com.acubenchik.leetcode;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/word-break/
+public class WordBreaks {
+
+    public static void main(String[] args) {
+        boolean res = new WordBreaks().wordBreak("catndog", Arrays.asList("cat",  "n", "dog"));
+        System.out.println(res);
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp =  new boolean[s.length()+1];
+        dp[0] = true;
+        for (int i = 1; i <=s.length(); i++) {
+            for (int j = i; j >=0; j--) {
+                if(dp[j]) {
+                    if(wordDict.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                    }
+                }
+            }
+        }
+        return dp[s.length()];
+
+    }
+
+
+}
