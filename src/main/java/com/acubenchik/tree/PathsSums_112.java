9039423c7a37da11fbc4639e0492f1fef5bdda82
acubenchik <alex.usavchenko@gmail.com>
2019-05-21 16:08:33 +0200

    []: some algorithms;

---
+++ src/main/java/com/acubenchik/tree/PathsSums_112.java
@@ -0,0 +1,14 @@
+package com.acubenchik.tree;
+
+public class PathsSums_112 {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null ) {
+          return false;
+        } else if(root.left == null && root.right == null && root.val - sum == 0) {
+            return true;
+        } else {
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+    }
+}
